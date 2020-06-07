0f3b8d211009d28d58cde19e36c75dc7b1a986d2
MagaFuture <18715253539@163.com>
2020-06-08 04:14:00 +0800

    new file:   assignment/VideoPlayer/.idea/codeStyles/Project.xml 	new file:   assignment/VideoPlayer/.idea/gradle.xml 	new file:   assignment/VideoPlayer/.idea/misc.xml 	new file:   assignment/VideoPlayer/.idea/runConfigurations.xml 	modified:   assignment/VideoPlayer/app/src/main/java/com/bytedance/videoplayer/MainActivity.java 	modified:   assignment/VideoPlayer/app/src/main/res/layout/activity_main.xml 	new file:   assignment/VideoPlayer/app/src/main/res/raw/bytedance.mp4 	new file:   "ppt/~$\345\244\232\345\252\222\344\275\223\345\237\272\347\241\200.pptx"

--- assignment/VideoPlayer/app/src/main/java/com/bytedance/videoplayer/MainActivity.java
+++ assignment/VideoPlayer/app/src/main/java/com/bytedance/videoplayer/MainActivity.java
@@ -1,19 +1,74 @@
 package com.bytedance.videoplayer;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
 import com.bumptech.glide.Glide;
 
 public class MainActivity extends AppCompatActivity {
+    private VideoView videoView;
+//    private int playProgress = 0;
+//    private boolean isPlaying = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ImageView imageView = findViewById(R.id.imageView);
-        String url = "https://s3.pstatp.com/toutiao/static/img/logo.271e845.png";
-        Glide.with(this).load(url).into(imageView);
+
+        videoView = findViewById(R.id.video);
+        Intent intent = getIntent();
+        Uri uri = intent.getData();
+        videoView.setVideoURI(uri);
+
+        MediaController mediaController = new MediaController(this);
+        videoView.setMediaController(mediaController);
+        mediaController.setMediaPlayer(videoView);
+
+        videoView.start();
     }
+
+    /*
+    @Override
+    protected void onPause(){
+        super.onPause();
+        playProgress = videoView.getCurrentPosition();
+        isPlaying = videoView.isPlaying();
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        videoView.seekTo(playProgress);
+        if (isPlaying)
+            videoView.start();
+    }
+
+    @Override
+    protected void onSaveInstanceState(@Nullable Bundle state){
+        if ( state != null ){
+            state.putInt("playProgress", playProgress);
+            state.putBoolean("isPlaying", isPlaying);
+        }
+        super.onSaveInstanceState(state);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(@Nullable Bundle state) {
+        super.onRestoreInstanceState(state);
+        if (state != null) {
+            int temp_PP = state.getInt("playProgress");
+            boolean temp_IP = state.getBoolean("isPlaying");
+            playProgress = temp_PP;
+            videoView.seekTo(temp_PP);
+            isPlaying = temp_IP;
+            if (isPlaying)
+                videoView.start();
+        }
+    }*/
 }
